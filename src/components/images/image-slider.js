cdbfca71f52b816c23d9bb22cc3991136a1e540c
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-21 15:43:09 -0700

    finish  carousel component

--- src/components/images/image-slider.js
+++ src/components/images/image-slider.js
@@ -3,21 +3,35 @@ import React, {useState} from "react"
 
 import carouselImages from "../../mock-data/carousel-mock-imgs"
 
-const ImageSlider = () => {
-    const [currentImage, setCurrent] = useState(0)
-    const length = slides.length
+const ImageSlider = (props) => {
+    const [currentImage, setCurrentImage] = useState(0)
+    const length = props.slides.length
+
+    const nextSlide = () => {
+        setCurrentImage(currentImage === length - 1 ? 0 : currentImage + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrentImage(currentImage === 0 ? length -1 : currentImage -1)
+    }
+
   
-    const carousel = carouselImages.map(image => {
+    const carousel = carouselImages.map((image, index) => {
         return(
-            <div key={image.id}>
-                <img className="carousel-item" src={image.imgUrl} />
+            <div key={image.id} className={index === currentImage ? 'active slide' : 'slide'}>
+                {index === currentImage && (
+                    <img className="carousel-img" src={image.imgUrl} />
+                )}
             </div>
         )
     })
+    console.log(currentImage)
     
     return (
-        <div>
+        <div className="about-page-container">
             <div className="carousel-container">
+                <div className="left-arrow" aria-label="previous slide" onClick={prevSlide}>{"<"}</div>
+                <div className="right-arrow" aria-label="next slide" onClick={nextSlide}>{">"}</div>
                 {carousel}
             </div>
         </div>
--- src/components/pages/about-us.js
+++ src/components/pages/about-us.js
@@ -1,16 +1,34 @@
 import React from "react"
 
 import ImageSlider from "../images/image-slider"
+import carouselImages from "../../mock-data/carousel-mock-imgs"
+import florist from "../../../static/images/about-page/florist.png"
 
 function AboutUs() {
     
     return (
-        <div>
-            <h1>Made it to about us</h1>
-            <ImageSlider />
+        <div className="about-page-wrapper">
+            <ImageSlider slides={carouselImages}/>
+            <div className="mission-statement-container">
+                <div className="about-us-background-div-img"
+                    style={{
+                        background: "url("+ florist +") no-repeat",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center"
+                    }}
+                >
+                </div>
+                <div className="mission-statement-box-container">
+                    <div className="mission-statement-title">
+                        TITLE
+                    </div>
+                    <div className="mission-statement-message">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque.
+                    </div>
+                </div>
+            </div>
         </div>
     )
-    
 }
 
 export default AboutUs
