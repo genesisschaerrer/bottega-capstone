4290eff28c0589aad44e2ad99771679b8e7d5455
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-26 14:25:43 -0700

    fetching from db and updating state

--- src/components/pages/shop-all.js
+++ src/components/pages/shop-all.js
@@ -1,23 +1,40 @@
-import React from "react"
+import React, {useState} from "react"
+import axios from "axios"
 import products from "../../mock-data/mock-data"
 
 
 
-function ShopAll() {
-     const productDisplay = products.map(product => {
+
+
+const ShopAll = () => {
+    const [products, setProducts] = useState([])
+
+    const getAllProducts = () => {
+        axios.get("http://localhost:4000/")
+            .then(response => {
+               return setProducts(response.data)
+            })
+    }
+
+    const renderProducts = () => {
         return (
-            <div className="product-card" key={product.id}>
-                <div className="product-name">{product.name}</div>
-                <img className="card-img" src={product.image} />
-                <div className="product-price">${product.price}.00</div>
-            </div>
-        )
-    })
-        return (
-            <div className="product-container">
-                {productDisplay}
-            </div>
-        )
+            products.map(product => {
+                return (
+                    <div className="product-card" key={product.id}>
+                        <div className="product-name">{product.name}</div>
+                        <img className="card-img" src={product.imageUrl} />
+                        <div className="product-price">${product.price}.00</div>
+                    </div> 
+                )
+            })     
+        )          
+    }
+
+    return (
+        <div className="product-container" getAllProducts={getAllProducts()}>
+            {renderProducts()}
+        </div>
+    )
 }
 
 export default ShopAll
