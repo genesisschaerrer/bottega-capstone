890f199559e9c4551f3b3f36b5614ecf5e3979c5
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-29 10:23:24 -0700

    added login functionality, right now working with a hardcoded value

--- src/components/pages/admin-login.js
+++ src/components/pages/admin-login.js
@@ -1,23 +1,53 @@
-import React from "react"
+import React, {useState} from "react"
+import { useHistory } from "react-router-dom"
 
 const AdminLogin = () => {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [loggedIn, setLoggedIn] = useState(false)
+    const history = useHistory()
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(username === "user" && email === "gen@gmail" && password === "12345678"){
+            setLoggedIn(true)
+            setUsername("")
+            setEmail("")
+            setPassword("")
+            console.log("im logged in")
+            history.push("/admindashboard")
+
+        } else {
+            console.log("wrong credentials")
+        }
+     
+    }
+
+
     return (
         <div className="admin-login-container">
             <h1 className="admin-header">ADMIN LOGIN</h1>
-            <form className="admin-login-form">
+            <form className="admin-login-form" onSubmit={handleSubmit}>
     
                 <input 
                 className="login-input-box"
                 type="text"
                 name="username"
                 placeholder="username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
                 />
 
                 <input 
                 className="login-input-box"
-                type="text"
+                type="email"
                 name="email"
                 placeholder="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
                 />
 
                 <input 
@@ -25,9 +55,11 @@ const AdminLogin = () => {
                 type="password"
                 name="password"
                 placeholder="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
                 />
-
-                <button>Submit</button>
+                {/* {loggedIn ? <Link to="/admindashboard" type="submit">Submit</Link>: <button type="submit">Submit</button>} */}
+                <button type="submit">Submint</button>
             </form>
 
         </div>
--- src/components/pages/cart.js
+++ src/components/pages/cart.js
@@ -4,19 +4,23 @@ import {CartContext} from "../context/cart-context"
 
 const Cart = () => {
     const [cart, setCart] = useContext(CartContext)
-    let orderSummary
+    let totalPrice = cart.map(cartItem => cartItem.price)
+    totalPrice = totalPrice.reduce((acc, curr) => acc + curr, 0)
+
 
     const removeCartItem = (id) => {
+        console.log("hit remove function")
         let indexStart = cart.findIndex(x => x._id === id)
         let indexEnd = indexStart + 1
 
         cart.splice(indexStart, indexEnd)
+        console.log(cart)
     }
 
     useEffect(()=> {
-        orderSummary = cart.map(product => {
+        orderSummary = cart.map((product, idx) => {
             return (
-                <div key={product._key}>
+                <div key={idx}>
                     <div className="summary-product-name">{product.name}</div>
                     <div className="product-summary-wrapper">
                         <img className="product-thumbnail" src={product.imageUrl} alt={product.name} />
@@ -30,29 +34,41 @@ const Cart = () => {
 
     console.log(cart)
     
-    // let orderSummary = cart.map(product => {
-    //     return (
-    //         <div key={product._key}>
-    //             <div className="summary-product-name">{product.name}</div>
-    //             <div className="product-summary-wrapper">
-    //                 <img className="product-thumbnail" src={product.imageUrl} alt={product.name} />
-    //                 <div className="prduct-price">{product.price}</div>
-    //                 <div className="remove-cart-item" onClick={() => removeCartItem(product._id)}>Delete Me</div>
-    //             </div>
-    //         </div>
-    //     )
-    // })
+    let orderSummary = cart.map((product, idx) => {
+        return (
+            <div key={idx}>
+                <div className="summary-product-name">{product.name}</div>
+                <div className="product-summary-wrapper">
+                    <img className="product-thumbnail" src={product.imageUrl} alt={product.name} />
+                    <div className="prduct-price">{`$${product.price}.00`}</div>
+                    <div className="remove-cart-item" onClick={() => removeCartItem(product._id)}>Delete Me</div>
+                </div>
+            </div>
+        )
+    })
 
     return (
         <div className="checkout-review-container">
-            <h1 className="cart-header">CHECKOUT REVIEW</h1>
+            <h1 className="cart-header">ORDER SUMMARY</h1>
             <div className="order-summary-container">
                 {orderSummary}
+                {/* {cart.map(product => {
+                    return (
+                        <div key={product._key}>
+                            <div className="summary-product-name">{product.name}</div>
+                            <div className="product-summary-wrapper">
+                                <img className="product-thumbnail" src={product.imageUrl} alt={product.name} />
+                                <div className="prduct-price">{product.price}</div>
+                                <div className="remove-cart-item" onClick={() => removeCartItem(product._id)}>Delete Me</div>
+                            </div>
+                        </div>
+                    )
+                })} */}
+
+                <div className="total-price">TOTAL PRICE: ${totalPrice}.00</div>
+
+                <button className="place-order-btn">PLACE ORDER</button>
             </div>
-
-
-            <div>TOTAL PRICE: $0.00</div>
-            <button>PLACE ORDER</button>
         </div>
     )
 }
